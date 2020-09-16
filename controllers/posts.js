4ed8d19bc4feb50bbe9058d25c94ba989438363a
Alejandro Barrera Gutierrez <63157914+alejobarrera@users.noreply.github.com>
2020-09-16 07:47:05 -0600

    Add Add list, show, edit and delete options in news

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,42 +1,23 @@
+//Database: finalproject
+//Collection: posts
 var express = require('express');
 var Post = require('../models/post');
 var fs = require('fs');
-
-exports.list = function(req, res) {
+//Show a list of items saved on the database
+exports.listItems = function(req, res) {
     Post.find(function (err, posts) {
         if (err) console.log(err)
         
         console.log(posts)
-        res.render('posts/list', { title: 'News', posts: posts });
+        res.render('posts/list-posts', { title: 'News', posts: posts });
     });
 };
-
-exports.addpostForm = function(req, res) {
+//Show a form to add posts and save them on the database
+exports.AddItem = function(req, res) {
     res.render('posts/add-post', { title: 'New post', post: {}, errors: [] });
 };
-
-exports.table = function(req, res) {
-    Post.find(function (err, posts) {
-        if (err) console.log(err)
-        
-        console.log(posts)
-        res.render('posts/table-post', { title: 'All posts', posts: posts });
-    });
-};
-
-exports.fulltext = function(req, res) {
-    var id = req.params.id;
-
-    console.log(id);
-    Post.findById(id, function (err, post) {
-        if (err) console.log(err)
-        
-        console.log(post)
-        res.render('posts/fulltext', { title: 'Fulltext', post: post });
-    });
-};
-
-exports.postCreate = function(req, res) {
+//Save the new post on the database
+exports.CreateItem = function(req, res) {
     var headline = req.body.headline;
     var abstract = req.body.abstract;
     var image = req.body.image;
@@ -45,7 +26,6 @@ exports.postCreate = function(req, res) {
     var authoremail = req.body.authoremail;
     var source = req.body.source;
     var sourceurl = req.body.sourceurl;
-
     var newPost = new Post({
         headline: headline,
         abstract: abstract,
@@ -66,5 +46,70 @@ exports.postCreate = function(req, res) {
           console.log('Post saved successfully!');
       }
 
+    });
+};
+//Show a table with the items saved on the database for admin purposes
+exports.AdminItems = function(req, res) {
+    Post.find(function (err, posts) {
+        if (err) console.log(err)
+        
+        console.log(posts)
+        res.render('posts/table-posts', { title: 'All posts', posts: posts });
+    });
+};
+//Show full text for any user and option to delete for admin purposes
+exports.ShowItem = function(req, res) {
+    var id = req.params.id;
+
+    console.log(id);
+    Post.findById(id, function (err, post) {
+        if (err) console.log(err)
+        
+        console.log(post)
+        res.render('posts/show-post', { title: 'Fulltext', post: post });
+    });
+};
+//Delete an item from the database
+exports.DeleteItem = function(req, res) {
+    var id = req.params.id;
+
+    console.log(id);
+    Post.findById(id, function (err, post) {
+        if (err) console.log(err)
+        console.log(post)
+
+        post.remove(function (err){
+         if (err) console.log(err)
+         res.redirect('/posts/table-posts')
+        });        
+    });
+};
+//Show a form to edit the information and update it on the database for admin purposes
+exports.EditItem = function(req, res) {
+    var id = req.params.id;
+
+    console.log(id);
+    Post.findById(id, function (err, post) {
+        if (err) console.log(err)
+        console.log(post)
+
+        res.render('posts/edit-post', { 
+            title: 'User zone', 
+            subtitle:'Admin user here',
+            sectiontitle: 'Edit item - Posts',
+            post: post,
+            errors: [] 
+        });
+    });
+};
+//Update the information on the database
+exports.UpdateItem = function(req, res) {
+    var id = req.params.id;
+    var update = req.body;
+
+    Post.findByIdAndUpdate(id, update, function (err, post) {
+        if (err) console.log(err)
+        console.log(post)
+        res.redirect('/posts/table-posts')
     });
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,22 +1,30 @@
+//Database: finalproject
+//Collection: posts
 var express = require('express');
 var postsController = require('../controllers/posts');
 var router = express.Router();
-
+//Show a list of items saved on the database
 router 
   .route('/')
-  .get(postsController.list)
-
+  .get(postsController.listItems)
+//Show a form to add posts and save them on the database
 router
   .route('/add-post')
-  .get(postsController.addpostForm)
-  .post(postsController.postCreate)
-
+  .get(postsController.AddItem)
+  .post(postsController.CreateItem)
+//Show a table with the items saved on the database for admin purposes
   router 
   .route('/table-posts')
-  .get(postsController.table)
-
+  .get(postsController.AdminItems)
+//Show full text for any user and option to delete for admin purposes
 router
-  .route('/:id')
-  .get(postsController.fulltext)
+  .route('/post/:id')
+  .get(postsController.ShowItem)
+  .delete(postsController.DeleteItem)
+//Show a form to edit the information and update it on the database for admin purposes
+router
+  .route('/post/:id/edit')
+  .get(postsController.EditItem)
+  .put(postsController.UpdateItem)
 
 module.exports = router;
