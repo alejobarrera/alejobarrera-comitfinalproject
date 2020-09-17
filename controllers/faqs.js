7fcba7b0b0357bd9d3914da650ece61077800a38
Alejandro Barrera Gutierrez <63157914+alejobarrera@users.noreply.github.com>
2020-09-16 18:05:18 -0600

    Add list, show, edit and delete options in faqs

--- controllers/faqs.js
+++ controllers/faqs.js
@@ -1,37 +1,28 @@
+//Database: finalproject
+//Collection: faqs
 var express = require('express');
 var Faq = require('../models/faq');
-
-exports.list = function(req, res) {
+//Show a list of items saved on the database
+exports.listItems = function(req, res) {
     Faq.find(function (err, faqs) {
         if (err) console.log(err)
         
         console.log(faqs)
-        res.render('faqs/list', { title: 'FAQs', faqs: faqs });
+        res.render('faqs/list-faqs', { title: 'FAQs', faqs: faqs });
     });
 };
-
-exports.addForm = function(req, res) {
+//Show a form to add faqs and save them on the database
+exports.AddItem = function(req, res) {
     res.render('faqs/add-faq', { title: 'New FAQ', faq: {}, errors: [] });
 };
-
-exports.table = function(req, res) {
-    Faq.find(function (err, faqs) {
-        if (err) console.log(err)
-        
-        console.log(faqs)
-        res.render('faqs/table-faqs', { title: 'All faqs', faqs: faqs });
-    });
-};
-
-exports.faqCreate = function(req, res) {
+//Save the new faq on the database
+exports.CreateItem = function(req, res) {
     var question = req.body.question;
     var answer = req.body.answer;
-
     var newFaq = new Faq ({
         question: question,
         answer: answer,
     });
-
     newFaq.save(function(err) {
       if (err) {
           res.render('faqs/add-faq', { faq: newFaq, errors: err.errors });
@@ -40,6 +31,60 @@ exports.faqCreate = function(req, res) {
           res.redirect('table-faqs');
           console.log('Faq saved successfully!');
       }
-
     });
 };
+
+//Show a table with the items saved on the database for admin purposes
+exports.AdminItems = function(req, res) {
+    Faq.find(function (err, faqs) {
+        if (err) console.log(err)
+        
+        console.log(faqs)
+        res.render('faqs/table-faqs', { title: 'All faqs', faqs: faqs });
+    });
+};
+//Delete an item from the database
+exports.DeleteItem = function(req, res) {
+    var id = req.params.id;
+
+    console.log(id);
+    Faq.findById(id, function (err, faq) {
+        if (err) console.log(err)
+        console.log(faq)
+
+        faq.remove(function (err){
+         if (err) console.log(err)
+         res.redirect('/faqs/table-faqs')
+        });        
+    });
+};
+//Show a form to edit the information and update it on the database for admin purposes
+exports.EditItem = function(req, res) {
+    var id = req.params.id;
+
+    console.log(id);
+    Faq.findById(id, function (err, faq) {
+        if (err) console.log(err)
+        console.log(faq)
+
+        res.render('faqs/edit-faq', { 
+            title: 'User zone', 
+            subtitle:'Admin user here',
+            sectiontitle: 'Edit item - Faqs',
+            faq: faq,
+            errors: [] 
+        });
+    });
+};
+//Update the information on the database
+exports.UpdateItem = function(req, res) {
+    var id = req.params.id;
+    var update = req.body;
+
+    Faq.findByIdAndUpdate(id, update, function (err, faq) {
+        if (err) console.log(err)
+        console.log(faq)
+        res.redirect('/faqs/table-faqs')
+    });
+};
+
--- routes/faqs.js
+++ routes/faqs.js
@@ -1,18 +1,29 @@
+//Database: finalproject
+//Collection: faqs
 var express = require('express');
 var faqsController = require('../controllers/faqs');
 var router = express.Router();
-
+//Show a list of items saved on the database
 router 
   .route('/')
-  .get(faqsController.list)
-
+  .get(faqsController.listItems)
+//Show a form to add faqs and save them on the database
 router
   .route('/add-faq')
-  .get(faqsController.addForm)
-  .post(faqsController.faqCreate)
-
+  .get(faqsController.AddItem)
+  .post(faqsController.CreateItem)
+//Show a table with the items saved on the database for admin purposes
   router 
   .route('/table-faqs')
-  .get(faqsController.table)
+  .get(faqsController.AdminItems)
+//Show delete option for admin purposes
+router
+  .route('/faq/:id')
+  .delete(faqsController.DeleteItem)
+//Show a form to edit the information and update it on the database for admin purposes
+router
+  .route('/faq/:id/edit')
+  .get(faqsController.EditItem)
+  .put(faqsController.UpdateItem)
 
 module.exports = router;
